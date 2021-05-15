10d0233850f0f0f56d98af36377d641dc3fb1dc7
Eryk Smoleń <eryk.smolenx@gmail.com>
2021-05-15 20:12:20 +0200

    Program kredytowy

---
+++ Kredyt.py
@@ -0,0 +1,125 @@
+kredyt = int(12000)
+miesieczna_rata = int(200)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * kredyt - miesieczna_rata
+
+print("Styczeń: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Luty: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Marzec: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Kwiecień: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Maj: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Czerwiec: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Lipiec: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Sierpień: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Wrzesień: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Październik: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Listopad: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Grudzień: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Styczeń: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Luty: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Marzec: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Kwiecień: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Maj: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Czerwiec: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Lipiec: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Sierpień: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Wrzesień: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Październik: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Listopad: ", wzor_raty)
+
+inflacja = float(input())
+wzor_raty = (1 + ((inflacja + 3) / 1200)) * wzor_raty - miesieczna_rata
+
+print("Grudzień: ", wzor_raty)
+
+
+print("Chyba działa")
